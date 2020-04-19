b6b55bea7a2f6849af8e72c04a1ac1bcb31e5295
itkhan <itkhanz@gmail.com>
2020-04-19 04:50:37 +0200

    Third Commit: Fixed Mobile Nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -90,6 +90,24 @@ $(document).ready(function() {
     });
     
     
+    /* Fixed Mobile Navigation Issue */
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+        nav.css("display", "block");
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+        } else {
+        nav.css("display", "none");
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+        }
+
+    });
+
+    
 });
 
 
